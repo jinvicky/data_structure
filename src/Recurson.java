58898b92c56ab58175bb79d4cb55ac18a729c702
jinvicky <jinvicky@intube.kr>
2023-11-02 16:34:24 +0900

    Feat: 정리 문서 추가 테스트

--- src/Recurson.java
+++ src/Recurson.java
@@ -1,5 +1,13 @@
+package src;
+
 public class Recurson {
 
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Recurson r = new Recurson();
+        r.recurse(5);
+    }
     public void recurse (int count) {
         if (count <=0)
             System.out.println(".");
